afab147d4b4f9a5a48eff54d09eba803928faea0
arsanyjoseph <83728833+arsanyjoseph@users.noreply.github.com>
2022-03-10 20:24:39 +0200

    routes

--- frontend/main.js
+++ frontend/main.js
@@ -1,30 +1,27 @@
-const try1 = '<p> Hello Worldfdskjfhksdhfkjsdhfjksdhfkjsdhfkshdfksdhfksdhfsldh sdhfksdhfl </p>'
-const healthy_foods = `<h2>The Definitive Guide to Healthy Eating in Real Life</h2>
-<section>
-    <p>Depending on whom you ask, “healthy eating” may take any number of forms. It seems that everyone, including healthcare professionals, wellness influencers, coworkers, and family members, has an opinion on the healthiest way to eat.
-        Plus, nutrition articles that you read online can be downright confusing with their contradictory — and often unfounded — suggestions and rules.
-        This does not make it easy if you simply want to eat in a healthy way that works for you.
-        The truth is, healthy eating does not have to be complicated. It is entirely possible to nourish your body while enjoying the foods you love.
-        After all, food is meant to be enjoyed — not feared, counted, weighed, and tracked.
-        This article cuts through the noise to explain what healthy eating means and how to make it work for you.</p>
-        <img id='mainArtImg' src="https://www.helpguide.org/wp-content/uploads/calories-counting-diet-food-control-and-weight-loss-concept-calorie-768.jpg" alt="food" />
-</section>
-<section>
-    <p>To read Full Article Click <a href="https://www.healthline.com/nutrition/how-to-eat-healthy-guide#importance" target= _blank>Here</a></p>
-</section>`
-const myFunction = () => {
-    history.pushState(null, '', '/healthy_foods')
-    document.getElementById('mainPage').innerHTML = healthy_foods
-}
+const router = async () => {
+    const routes = [
+        { path: '/' , view: ()=> console.log('healthyfoods')},
+        { path: '/exercises' , view: ()=> console.log('exercises')},
+        { path: '/lifestyle' , view: ()=> console.log('lifestyle')},
+        { path: '/quotes' , view: ()=> console.log('quotes')}
+    ]
 
-document.getElementById('first').addEventListener("click", myFunction)
+    const matchRoutes = routes.map(route => {
+        return {
+            route: route,
+            isMatching: location.pathname === route.path
+        };
+    });
+    let matched = matchRoutes.find(matchRoutes => matchRoutes.isMatching ===true)
+    if (!matched) {
+        matched = {
+            route: routes[0],
+            isMatching: true
+        }
+    }
+    console.log(matched)
+};
 
-const try2 = '<p> Hello World 2</p>'
-
-const myFunction2 = () => {
-    history.pushState(null, '', '/lol')
-    document.getElementById('mainPage').innerHTML = try2
-}
-
-document.getElementById('second').addEventListener("click", myFunction2)
-
+document.addEventListener("DOMContentLoaded", ()=> {
+    router();
+})
